dc30bef089e5dbee572be3878d1c357fb463aff4
sannypro <sannymr79@gmail.com>
2022-05-21 23:20:53 +0600

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div >
-      <button class="btn btn-active btn-primary">Button</button>
+      <Navbar></Navbar>
     </div>
   );
 }
